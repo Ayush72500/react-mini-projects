0c507523cee23e22a4eecdbe376102a187c6e7d1
Ayush Raj <ayushraj72500@gmail.com>
2026-08-02 16:21:13 +0530

    Add React counter app with Tailwind CSS

--- 01-counter-app/src/App.jsx
+++ 01-counter-app/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+function App(){
 
-  function increase() {
-    setCount(count + 1);
-  }
+   const [count, setCount] = useState(0)
 
-  function decrease() {
-    setCount(count - 1);
-  }
-
-  function reset() {
-    setCount(0);
-  }
-
-  return (
-    <div>
-      <h1>Counter App</h1>
-
-      <h2>{count}</h2>
-
-      <button onClick={increase}>+</button>
-
-      <button onClick={decrease}>-</button>
-
-      <button onClick={reset}>Reset</button>
-    </div>
-  );
+function increment() {
+  setCount((value)=>value+1);
+}
+function decrement() {
+  setCount((value)=>value-1);
+}
+function reset() {
+  setCount(0);
+}
+return(
+  <div className="flex flex-col items-center justify-center ">
+  <h1 className="text-6xl font-bold">Counter App</h1>
+  <h2 className="text-5xl my-6">{count}</h2>
+  <div className="flex gap-8 ">
+  <button className="bg-sky-300 text-white px-6 py-2 rounded-lg" onClick={increment}>+</button>
+  <button className="bg-sky-300 text-white px-6 py-2 rounded-lg" onClick={decrement}>-</button>
+  <button className="bg-sky-300 text-white px-6 py-2  rounded-lg" onClick={reset}>reset</button>
+  
+  </div>
+  </div>
+  
+  
+)
 }
 
 export default App;
