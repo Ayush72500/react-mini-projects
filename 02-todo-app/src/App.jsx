1a378cf7215540b93c45b25f1b6e415d01a6bc4c
Ayush Raj <ayushraj72500@gmail.com>
2026-08-02 23:11:22 +0530

    Updated todo

--- 02-todo-app/src/App.jsx
+++ 02-todo-app/src/App.jsx
@@ -1,16 +1,44 @@
 import {useState} from "react";
 
 function App(){
+const [input, setinput] = useState("")
+const [tasks, settasks] = useState([])
+
+function addtask(e) {
+  e.preventDefault();
+  if(input.trim()==="") return;
+  settasks([...tasks,input]);
+  setinput("");
+  
+}
+function deletee(index){
+  settasks(
+    tasks.filter((task,i) => {
+    return i !== index;
+  }))
+}
   return(
     <div>
       <h1>Todo App</h1>
     <form >
-      <input type="text" placeholder="enter the tasks" />
-      <button>Enter</button>
+      <input type="text" placeholder="enter the tasks" value={input} onChange={(e)=>setinput(e.target.value)}/>
+      <button onClick={addtask}>add</button>
     </form>
     
     <ul>
-      <li>Task1 <button>delete</button></li>
+      {
+        tasks.map((task, index)=>(
+          <div>
+            <li>{task}
+              <button onClick={()=>deletee(index)}>delete</button>
+            </li>
+          
+          </div>
+          
+        ))
+      }
+
+      
       
       
     </ul>
