57fb5ab1eacc823a5ae718351ac8038e8b9273de
Ayush Raj <ayushraj72500@gmail.com>
2026-08-08 14:54:14 +0530

    profile card added

---
+++ profile card/src/App.jsx
@@ -0,0 +1,15 @@
+import ProfileCard from "./ProfileCard";
+
+function App(){
+  return (
+    <div>
+      <h1>👤 Profile Image</h1>
+      <ProfileCard name="Ayush" role="Developer" age={22}/>    
+      
+         
+    </div>
+  )
+    
+}
+
+export default App;
